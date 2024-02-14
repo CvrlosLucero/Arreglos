2e01748e2606017ff39578a04b3c6bf530cefb8b
CvrlosLucero <carloslucerotobar001@gmail.com>
2024-02-14 14:49:38 -0500

    Create triki.cpp

---
+++ Matrices/triki.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    string triki[3][3];
+    string ficha;
+
+    for(i=0; i<3; i++){
+        for(j=0; j<3; j++){
+            cout<<"Pon X o O para la posición "<<"("<<i<<","<<j<<")"<<endl;
+            cin>>ficha;
+            triki[3][3] = ficha + triki[3][3];
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
