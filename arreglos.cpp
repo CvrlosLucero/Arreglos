d91181b8407f98d767580801b9a3f63d58693fb2
CvrlosLucero <carlosj.lucero@urosario.edu.co>
2024-03-31 13:23:38 -0500

    Added documentation
    Fixed segmentation fault in line 13.

--- arreglos.cpp
+++ arreglos.cpp
@@ -1,30 +1,31 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
 int main(){
 
+    //declarar arreglos
     string nombres[5]={"Carlos","Daniel","Luisa","Simon","Christian"};
     string nombres2[5];
 
-    //for(int i=0; i <= 5; i++){
-        //cout<<" "<<nombres2[k]<<endl;//
-
-
-
-    for(int j=0; j<=nombres2->length();j++){
-    cout<<"Ingrese los nombres para el arreglo: ";
-    string name;
-    cin>>name; // o cin>>nombres2[j]
-    nombres2[j] = name; 
+    //imprimir el primer arreglo (nombres)
+    cout<<"Nombres en el primer arreglo: "<<endl;
+    for(int i=0; i < 5; i++){
+        cout<<nombres[i]<<endl;
     }
 
-    for(int k=0; k <= nombres2->length(); k++){
+    //llenar el segundo arreglo (nombres2)
+    for(int j=0; j<5;j++){
+        cout<<"Ingrese los nombres para el arreglo: ";
+        string name;
+            cin>>name; // o cin>>nombres2[j]
+        nombres2[j] = name; 
+    }
 
+    //imprimir el segundo arreglo (nombres2)
+    for(int k=0; k < 5; k++){
         cout<<nombres2[k]<<endl;
     }
     
-
     return 0;
 }
