26686fedbbc8a1988f83ea710400cc3af47b5004
CvrlosLucero <carlosj.lucero@urosario.edu.co>
2024-03-31 13:59:51 -0500

    Added documentation
    Also added a quick function to print the vector.

--- Matrices/triki.cpp
+++ Matrices/triki.cpp
@@ -3,32 +3,26 @@
 using namespace std;
 
 int main(){
-
+    //declarar variables y vector
     string triki[3][3];
     string ficha;
 
-    for(i=0; i<3; i++){
-        for(j=0; j<3; j++){
+    //llenar vector
+    for(int i=0; i<3; i++){
+        for(int j=0; j<3; j++){
             cout<<"Pon X o O para la posición "<<"("<<i<<","<<j<<")"<<endl;
-            cin>>ficha;
-            triki[3][3] = ficha + triki[3][3];
+                cin>>ficha;
+            triki[i][j] = ficha;
         }
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    //imprimir vector
+    for(int i=0; i<3; i++){
+        for(int j=0; j<3; j++){
+            cout<<triki[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    
     return 0;
 }
