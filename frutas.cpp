76dbc1e5274858cc8b34a6065aa2694a0ba7e983
CvrlosLucero <carlosj.lucero@urosario.edu.co>
2024-03-31 13:13:18 -0500

    Added documentation

--- frutas.cpp
+++ frutas.cpp
@@ -2,22 +2,25 @@
 
 using namespace std;
 
+
 int main(){
+    //declarar variables
     int cantidadFrutas;
 
+    //pedir cantidad de frutas
     cout<<"Cuantas frutas quiere en su arreglo: ";
     cin>>cantidadFrutas;
 
+    //crear vector de frutas
     string frutas[cantidadFrutas];
 
-   
-
+    //llenar vector de frutas
     for(int i=0;i<cantidadFrutas;i++){
-
-        cout<<"ingrese la fruta en la posición"<<i;
-        cin>>frutas[i];
+        cout<<"ingrese la fruta en la posición "<<i<<": ";
+            cin>>frutas[i];
     }
 
+    //imprimir vector de frutas
     for(int j=0; j<cantidadFrutas; j++){
         cout<<frutas[j]<<endl;
     }
